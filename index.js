d113c0348e6786998a3abe5d47edf2705e0f4d0e
Domiciano Rincón <domiciano.rincon@correounivalle.edu.co>
2024-05-06 09:13:06 -0500

    Semana 14

--- index.js
+++ index.js
@@ -4,25 +4,24 @@ import fetch from 'node-fetch';
 import express from 'express';
 import bodyparser from 'body-parser';
 
-
 const app = express();
 app.use(bodyparser.json())
 
-const port = 3000;
+const PORT = 3000;
+const PROJECT_NAME = 'facelogprueba';
+const KEY_FILE_NAME = 'fcmkey2.json';
 
 app.post('/fcm/send', async (req, res) => {
-    let response = await alfa(req.body);
+    let response = await sendPushNotification(req.body);
     res.status(200).send(response);
 });
   
-app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
-
-
-async function alfa(data){
-    const credentials = JSON.parse(fs.readFileSync('fcmkey2.json'));
+async function sendPushNotification(data){
+    const credentials = JSON.parse(fs.readFileSync(KEY_FILE_NAME));
     const auth = new google.auth.GoogleAuth({
         credentials,
         scopes: 'https://www.googleapis.com/auth/firebase.messaging',
@@ -30,7 +29,7 @@ async function alfa(data){
     const authToken = await auth.getAccessToken();
     console.log(authToken);
 
-    let response = await fetch('https://fcm.googleapis.com/v1/projects/facelogprueba/messages:send', {
+    let response = await fetch(`https://fcm.googleapis.com/v1/projects/${PROJECT_NAME}/messages:send`, {
         method:'POST',
         headers:{
             'Content-Type':'application/json; UTF-8',
